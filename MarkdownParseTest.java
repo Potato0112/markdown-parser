e54deb31649b4f875601d994972cb5e395d0afb2
Potato0112 <103155948+Potato0112@users.noreply.github.com>
2022-04-23 16:01:53 -0700

    first edit of markdown parser

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ public class MarkdownParseTest {
 
     @Test
     public void getLinks() throws IOException{
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("othertests/test-file8.md");
         String content = Files.readString(fileName);
         ArrayList<String> compare = new ArrayList<>();
         compare.add("https://something.com");
